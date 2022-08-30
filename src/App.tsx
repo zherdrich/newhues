12b8900eb946ef4ec0f7fb9fe867997b880e1b6c
zherdrich <z.herdrich98@gmail.com>
2022-08-29 22:21:20 -0400

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import John from "./components/test1"
+import Home from './components/home';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, NavLink } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <John/>
-      <div>yo wassup dude</div> 
-      
+      <Home/>
     </div>
   );
 }
---
+++ src/components/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div>welcome to</div>
+      <div> newhues</div>
+    </div>
+  );
+}

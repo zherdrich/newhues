775e1d2f03e7ed0ae8816212f85a55f326781d43
zherdrich <z.herdrich98@gmail.com>
2022-08-30 12:07:36 -0400

    commit2

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./home.css";
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div>
-      <div>welcome to</div>
-      <div> newhues</div>
+    <div className="home">
+      <div className="welcome">welcome to</div>
+      <div className="title"> newhues</div>
+      <div className="colorblock">||||</div>
+      <button className="enter">enter</button>
     </div>
   );
 }

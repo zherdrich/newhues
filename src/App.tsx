6885e1adf14d83adb494cfbb31f8964182507db8
zherdrich <z.herdrich98@gmail.com>
2022-08-30 12:52:30 -0400

    commit3 nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link, NavLink } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, NavLink } from 'react-router-dom'
+import Quiz from './components/quiz';
+import Result from './components/result';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+            <Router>
+
+        <a href="/home" >Start over</a>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,12 +3,15 @@ import "./home.css";
 import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
   return (
     <div className="home">
       <div className="welcome">welcome to</div>
       <div className="title"> newhues</div>
       <div className="colorblock">||||</div>
-      <button className="enter">enter</button>
+      <div className="start" onClick={() => {navigate("/quiz")}}>start</div>
     </div>
   );
 }

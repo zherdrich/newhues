4e9f630aa370f4888594257bf763b5c9978fa6eb
zherdrich <z.herdrich98@gmail.com>
2022-12-04 16:39:42 -0500

    commit dec4.2?

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,21 +1,26 @@
 import "./search.css"
-import { Search } from "../models/id-models";
+import getAlbum from "../services/getAlbum";
+import { useState } from "react";
 
 
 
-export default function Search() {
+export default function SearchBar() {
+
+    const [query, setQuery] = useState("");
+
+
+
   return (
     <div className="search">
         <div className="main">
 
 
       <div className="searchBar">
-        <input
-          className="searchbar"
-          id="searchbar"
-          type="text"
-          placeholder="Search..."
-        />
+        <input className="searchbar" id="searchbar" type="text" placeholder="Search..." 
+        onChange={(e) => setQuery(e.target.value)}/>
+        <ul className="list">
+            {getAlbum.map((album) => ( <li className="listItem">{search.query}</li> )) }
+        </ul>
       </div>
 
         </div>
--- src/services/getAlbum.ts
+++ src/services/getAlbum.ts
@@ -8,7 +8,7 @@ export default function getAlbum(
     query: string,
   ): Promise<Search[] | void> {
     return axios
-      .get<Results>(`https://api.discogs.com/artists/`, {
+      .get<Results>(`https://api.discogs.com/database/search?q=${query}`, {
         params: {
           apiKey: process.env.REACT_APP_API_KEY,
           query: query,

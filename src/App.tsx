2ebf54c70108497eb9cfe3140270ac3b0bdea161
zherdrich <z.herdrich98@gmail.com>
2022-12-17 14:07:58 -0500

    commit dec17.3

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Home from './components/home';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link, NavLink } from 'react-router-dom'
 import Result from './components/result';
 import Search from './components/search';
-import { useState } from 'react';
 import express from 'express'
 import path from 'path';
+import { useEffect, useState } from 'react';
 
 
 
@@ -18,18 +18,33 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+  
+
+  if (!token && hash) {
+    token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))!.split("=")[1]
+    
+    console.log(token)
+  }
+
+}, [])
+
 
 
   return (
     <div className="App">
-            <Router>
+            {/* <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={} />
           <Route path="/search" element={<Search/>}/>
           <Route path="/result" element={<Result />} />
           <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
-      </Router>
+      </Router> */}
 
       <div className="home">
       <div className="main">
@@ -41,7 +56,7 @@ function App() {
           and organize your 
           favorites and soon-to-bes
           all in one place</div>
-<a className="start" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`}>
+<a className="start" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
   <div>login</div>
 </a>
       </div>

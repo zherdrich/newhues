69ebce0157c08413ef5a647c3918a1c6125b1136
zherdrich <z.herdrich98@gmail.com>
2022-12-29 16:12:32 -0500

    dec29.1

---
+++ src/Login.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=56f79d1a09c34459a7514616e29ec33c&response_type=code&redirect_uri=http://localhost3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
